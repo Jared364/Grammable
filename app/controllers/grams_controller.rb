25e2641a3102814b425c4a84f96f4f305cf8ce3b
Jared364 <jaredand1994@gmail.com>
2020-01-26 22:01:34 +0000

    added gram show page with testing for successful page post

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -1,6 +1,15 @@
 class GramsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
+
   def index
 
+  end
+
+  def show
+    @gram = Gram.find_by_id(params[:id])
+    if @gram.blank?
+      render plain: 'Not Found :(', status: :not_found
+    end
   end
 
   def new
@@ -8,8 +17,12 @@ class GramsController < ApplicationController
   end
 
   def create
-    @gram = Gram.create(gram_params)
-    redirect_to root_path
+    @gram = current_user.grams.create(gram_params)
+    if @gram.valid?
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   private
